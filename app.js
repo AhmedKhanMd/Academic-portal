a2dd3a53cc5a90a5c3ae37b440a5ef282d32bb41
balajipulugujju <balajipulugujju23@gmail.com>
2021-09-24 16:12:55 +0530

    namg

--- app.js
+++ app.js
@@ -2,25 +2,86 @@ const express = require("express");
 const bodyParser = require("body-parser"); 
 const ejs = require("ejs");
 
+const mongoose=require("mongoose");
+const session = require('express-session');
+const passport=require("passport");
+const passportLocalMongoose=require("passport-local-mongoose");
+ 
+const findOrCreate=require('mongoose-findorcreate');
 const app = express(); 
 
 app.set('view engine', 'ejs'); 
 app.use(bodyParser.urlencoded({extended:true})); 
 app.use(express.static("public")); 
 
+ 
+app.use(session({
+    secret:"sravanWEBdevResearchPublication.",
+    resave: false,
+    saveUninitialized: true
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  mongoose.connect("mongodb://localhost:27017/studytodayDB",{useNewUrlParser:true,useUnifiedTopology: true});
+   
+   
+  const userSchema= new mongoose.Schema({
+      email:String,
+      password:String
+      
+      
+    });
+    userSchema.plugin(passportLocalMongoose);
+    
+    userSchema.plugin(findOrCreate);
+  const User=new mongoose.model("User",userSchema);
+  passport.use(User.createStrategy());
+  passport.serializeUser(function(user, done) {
+      done(null, user.id);
+    });
+    
+    passport.deserializeUser(function(id, done) {
+      User.findById(id, function(err, user) {
+        done(err, user);
+      });
+    });
+
 app.get("/", function(req, res){
     res.render("index");
 }) 
 
-app.post("/signup", function(req, res){
+app.post("/signup", function(req, res){ 
+    User.register({username: req.body.Email}, req.body.Password, function(err, user){
+        if(err){
+            console.log(err);  
+            res.redirect("/");
+        } 
+        else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/academics");
+            }); 
+        }
+    });
+});
 
-    res.render("home");
-})
+app.post("/login", function(req, res){
+    const newUser = new User({
+        email: req.body.Email, 
+        password: req.body.Password
+    }); 
 
-app.post("/login",function(req, res){
-
-    res.render("home"); 
-}) 
+    req.login(newUser, function(err){
+        if(err){
+            console.log(err);  
+            res.redirect("/");
+        } 
+        else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/home");
+            }); 
+        }
+    })
+});
 
 app.get("/home",function(req, res){
 
