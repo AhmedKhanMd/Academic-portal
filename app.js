7f35b7d9d492b8f2b65f009dfad43a239ca4a7e2
balajipulugujju <balajipulugujju23@gmail.com>
2021-09-25 22:58:41 +0530

    firebase

--- app.js
+++ app.js
@@ -1,86 +1,56 @@
 const express = require("express"); 
 const bodyParser = require("body-parser"); 
 const ejs = require("ejs");
+const firebase = require("firebase");
+const { get } = require("express/lib/response");
 
-const mongoose=require("mongoose");
-const session = require('express-session');
-const passport=require("passport");
-const passportLocalMongoose=require("passport-local-mongoose");
- 
-const findOrCreate=require('mongoose-findorcreate');
 const app = express(); 
 
 app.set('view engine', 'ejs'); 
 app.use(bodyParser.urlencoded({extended:true})); 
 app.use(express.static("public")); 
 
- 
-app.use(session({
-    secret:"sravanWEBdevResearchPublication.",
-    resave: false,
-    saveUninitialized: true
-  }));
-  app.use(passport.initialize());
-  app.use(passport.session());
-  mongoose.connect("mongodb://localhost:27017/studytodayDB",{useNewUrlParser:true,useUnifiedTopology: true});
-   
-   
-  const userSchema= new mongoose.Schema({
-      email:String,
-      password:String
-      
-      
-    });
-    userSchema.plugin(passportLocalMongoose);
-    
-    userSchema.plugin(findOrCreate);
-  const User=new mongoose.model("User",userSchema);
-  passport.use(User.createStrategy());
-  passport.serializeUser(function(user, done) {
-      done(null, user.id);
-    });
-    
-    passport.deserializeUser(function(id, done) {
-      User.findById(id, function(err, user) {
-        done(err, user);
-      });
-    });
+const firebaseConfig = {
+    apiKey: "AIzaSyApBWwJHhpvdhHITOHOuy42x_fgxhlrU1Y",
+    authDomain: "studytoday-608a2.firebaseapp.com",
+    projectId: "studytoday-608a2",
+    storageBucket: "studytoday-608a2.appspot.com",
+    messagingSenderId: "169026371704",
+    appId: "1:169026371704:web:f96fc2d3f6a8a78e40ed8d"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp; 
+const auth = firebase.auth(); 
 
 app.get("/", function(req, res){
-    res.render("index");
+    res.render("index",{message:""});
 }) 
 
 app.post("/signup", function(req, res){ 
-    User.register({username: req.body.Email}, req.body.Password, function(err, user){
-        if(err){
-            console.log(err);  
-            res.redirect("/");
-        } 
-        else{
-            passport.authenticate("local")(req, res, function(){
-                res.redirect("/academics");
-            }); 
-        }
+    const email = req.body.Email; 
+    const password = req.body.Password; 
+    auth.createUserWithEmailAndPassword(email, password).then(cred =>{
+        console.log("user created"); 
+        res.render("index",{message:"User created, please login"});
+    }).catch(error=>{
+        console.log(error.code); 
+        res.render("index",{message:"User Not created"});
     });
 });
 
 app.post("/login", function(req, res){
-    const newUser = new User({
-        email: req.body.Email, 
-        password: req.body.Password
-    }); 
-
-    req.login(newUser, function(err){
-        if(err){
-            console.log(err);  
-            res.redirect("/");
-        } 
-        else{
-            passport.authenticate("local")(req, res, function(){
-                res.redirect("/home");
-            }); 
-        }
-    })
+    const email = req.body.Email; 
+    const password = req.body.Password; 
+    auth.signInWithEmailAndPassword(email, password).then(cred =>{
+        console.log("user loged in"); 
+        res.redirect("/home");
+    }).catch(error=>{ 
+        console.log(error.code); 
+        res.render("index",{message:"INVALID CREDENTIALS"});
+    });
 });
 
 app.get("/home",function(req, res){
